809948e3e56ed631a93a285595dbe55f74ccc107
serehin-denis <serehindenis@gmail.com>
2026-03-12 13:42:22 +0200

    update: added idle animation

--- components/player.gd
+++ components/player.gd
@@ -1,21 +1,26 @@
 extends CharacterBody2D
 
+# параметры персонажа
+const SPEED = 450.0
+const JUMP_VELOCITY = -600.0
+const GRAVITY_MULTIPIER = 2.2
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+# 
 func flip_img(direction):
-	if direction > 0: $Sprite2D.scale.x = 1
-	elif direction < 0: $Sprite2D.scale.x = -1
+	if direction > 0: $AnimatedSprite2D.scale.x = 1
+	elif direction < 0: $AnimatedSprite2D.scale.x = -1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y > 0: velocity += get_gravity() * delta * GRAVITY_MULTIPIER
+		else: velocity += get_gravity() * delta
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0: velocity.y /= 2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
